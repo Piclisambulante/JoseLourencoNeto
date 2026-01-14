9a97f4ac0500624304c49439d2e53de3a74e6e25
Jose Lourenço Neto <141178650+Piclisambulante@users.noreply.github.com>
2026-01-14 19:25:04 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
   server: {
+    host: "::",
+    port: 8080,
     allowedHosts: [
       'portifolio-f88e.onrender.com' 
     ]
-  }
-})
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      // ESSA É A PARTE QUE FALTA:
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
